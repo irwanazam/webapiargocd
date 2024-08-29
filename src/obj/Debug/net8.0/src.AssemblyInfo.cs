28960a9bbbace746810e1bafbfa904aeca65e88c
ryzamm <ryzamm@yahoo.com>
2024-08-29 09:35:07 +0800

    Add dockerfile

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4dfe30d6dff0a9a50a42ae64bcbf8d907f521ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81831bfbaa0a0bbf1b3c719b924e13e1cd2b8eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

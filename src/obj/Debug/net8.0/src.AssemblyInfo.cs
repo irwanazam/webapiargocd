4b1e9bbb73e817951d2050f2207c7888624f39b2
ryzamm <ryzamm@yahoo.com>
2024-08-29 09:01:31 +0800

    Update deployment

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bd12b0708f079503ba496404d2ab6dc87d29db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb2ad5dfebeafb73ac4745eb841c4ad7bcd51cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
